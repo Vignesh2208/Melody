6557900ecb44541a03d7275602ed1f9ef954c253
Kartik Palani <kartikp@mit.edu>
2017-01-17 13:01:09 -0600

    Added more network features.Need to try adding DNP3

--- anomaly_detection/src/network_features.py
+++ anomaly_detection/src/network_features.py
@@ -1,7 +1,7 @@
 import dpkt
 import socket
 
-#filename='/Users/kartik/Desktop/Siebel Project/microgrid_with_background_traffic/s3-eth5-s5-eth2.pcap'
+filename='/Users/kartik/Desktop/Siebel Project/microgrid_with_background_traffic/icmp_trial.pcap'
 
 def dst_list(filename):
     dst_list = []
@@ -29,6 +29,7 @@ def dst_list(filename):
 
 
         i += 1
+        #print i
     return dst_list, start_ts, final_ts
 
 
@@ -46,6 +47,13 @@ def network_features(filename, dst_list, start, end):
       tcp_list = [0] * (int(end-start)+1)
       udp_list = [0] * (int(end-start)+1)
       byte_list = [0] * (int(end-start)+1)
+      http_byte_list = [0] * (int(end-start)+1)
+      ssh_byte_list = [0] * (int(end-start)+1)
+      telnet_byte_list = [0] * (int(end-start)+1)
+      dnp_byte_list = [0] * (int(end-start)+1)
+      scan_byte_list = [0] * (int(end-start)+1)
+      ping_byte_list = [0] * (int(end-start)+1)
+
       stats_list = []
 
       for ts, pkt in dpkt.pcap.Reader(open(filename,'r')):
@@ -55,12 +63,29 @@ def network_features(filename, dst_list, start, end):
              continue
 
           ip=eth.data
-
-          if ip.dst == ip_dst:
-            byte_list[int(ts-start)]+= len(pkt)
+          
 
           if ip.p==dpkt.ip.IP_PROTO_TCP and ip.dst == ip_dst:
             tcp_list[int(ts-start)]+=1
+
+            tcp = ip.data
+            if tcp.dport == 8000:
+              http_byte_list[int(ts-start)]+= len(pkt)
+
+            if tcp.dport == 22:
+              ssh_byte_list[int(ts-start)]+= len(pkt)
+
+            if tcp.dport == 23:
+              telnet_byte_list[int(ts-start)]+= len(pkt)
+
+            #if tcp.dport == 20000:
+              #dnp_byte_list[int(ts-start)]+= len(pkt)
+            
+          #if ip.dst == ip_dst:
+            #byte_list[int(ts-start)]+= len(pkt)
+
+          if ip.p==dpkt.ip.IP_PROTO_ICMP and ip.dst == ip_dst:
+            ping_byte_list[int(ts-start)]+= len(pkt)
 
 
           if ip.p==dpkt.ip.IP_PROTO_UDP and ip.dst == ip_dst:
@@ -68,9 +93,17 @@ def network_features(filename, dst_list, start, end):
 
       stats_list.append(tcp_list)
       stats_list.append(udp_list)
-      stats_list.append(byte_list)
+      #stats_list.append(byte_list)
+      stats_list.append(http_byte_list)
+      stats_list.append(ssh_byte_list)
+      stats_list.append(dnp_byte_list)
+      stats_list.append(telnet_byte_list)
+      stats_list.append(ping_byte_list)
       stats[socket.inet_ntoa(ip_dst)] = stats_list
 
     
 
   return stats
+
+dst_list, start_ts, final_ts = dst_list(filename)
+print network_features(filename,dst_list, start_ts, final_ts)
