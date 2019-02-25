290600ba18fc22e1115f48e7a5bc9a8fc163b5d6
kartik Palani <kartikpalani@wirelessprv-10-195-157-98.near.illinois.edu>
2019-02-25 17:12:06 -0600

    added mock to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['mininet', 'mininet.net', 'kronos_functions']
+MOCK_MODULES = ['mininet', 'mininet.net', 'mininet.node', 'mininet.link', 'kronos_functions']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
