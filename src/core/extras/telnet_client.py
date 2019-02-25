2e192f79a975127e7b8f3dc0dda7e760ca26164c
Vignesh Babu <vig2208@gmail.com>
2019-02-25 02:31:27 -0800

    updating sample password to project_melody

--- src/core/extras/telnet_client.py
+++ src/core/extras/telnet_client.py
@@ -11,7 +11,7 @@ def main():
 
     dest_ip = args.dest_ip
     username = "moses"
-    password = "davidmnicol"
+    password = "project_melody"
 
     tn = telnetlib.Telnet(dest_ip)
     tn.expect(["login: "])
