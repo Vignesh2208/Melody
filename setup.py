6f8870d26e8d72403763b498ff0ca25792172931
kartik Palani <kartikpalani@wirelessprv-10-195-157-98.near.illinois.edu>
2019-02-25 15:14:54 -0600

    Edited the setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 setup(name='shared_buf', version='1.0',  \
-      ext_modules=[Extension('shared_buf', include_dirs=['/usr/local/include','/usr/include'], libraries=['pthread','rt'], library_dirs = ['/usr/local/lib'], sources = ['src/core/libs/shared_buf.c','src/core/libs/utils/linkedlist.c','src/core/libs/utils/hashmap.c'])])
+      ext_modules=[Extension('shared_buf', extra_compile_args=['-o src/core/libs/'],include_dirs=['/usr/local/include','/usr/include'], libraries=['pthread','rt'], library_dirs = ['/usr/local/lib'], sources = ['src/core/libs/shared_buf.c','src/core/libs/utils/linkedlist.c','src/core/libs/utils/hashmap.c'])])
       
 setup(name='gettimepid', version='1.0',  \
-      ext_modules=[Extension('gettimepid', include_dirs=['/usr/local/include','/usr/include'], libraries=[], library_dirs = ['/usr/local/lib'], sources = ['src/core/libs/gettimepid.c'])])
+      ext_modules=[Extension('gettimepid', extra_compile_args=['-o src/core/libs/'], include_dirs=['/usr/local/include','/usr/include'], libraries=[], library_dirs = ['/usr/local/lib'], sources = ['src/core/libs/gettimepid.c'])])
